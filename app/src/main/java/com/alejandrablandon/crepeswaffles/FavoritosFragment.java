549182f3f64608e42ff25ee6241b69c57966ac1b
AlejandraBlandonRios <alejandra.blandon@udea.edu.co>
2016-10-24 21:42:44 -0500

    Version 4.0

--- app/src/main/java/com/alejandrablandon/crepeswaffles/FavoritosFragment.java
+++ app/src/main/java/com/alejandrablandon/crepeswaffles/FavoritosFragment.java
@@ -1,11 +1,11 @@
 package com.alejandrablandon.crepeswaffles;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 /**
@@ -13,17 +13,35 @@ import android.view.ViewGroup;
  */
 public class FavoritosFragment extends Fragment {
 
+    LinearLayout primero,segundo,tercero,cuarto,quinto,sexto;
+    View myView;
 
     public FavoritosFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        myView = inflater.inflate(R.layout.fragment_favoritos, container, false);
+        primero=(LinearLayout)myView.findViewById(R.id.primero);
+        segundo=(LinearLayout)myView.findViewById(R.id.segundo);
+        tercero=(LinearLayout)myView.findViewById(R.id.tercero);
+        cuarto=(LinearLayout)myView.findViewById(R.id.cuarto);
+        quinto=(LinearLayout)myView.findViewById(R.id.quinto);
+        sexto=(LinearLayout)myView.findViewById(R.id.sexto);
+
+        primero.setVisibility(View.VISIBLE);
+        segundo.setVisibility(View.GONE);
+        tercero.setVisibility(View.GONE);
+        cuarto.setVisibility(View.GONE);
+        quinto.setVisibility(View.GONE);
+        sexto.setVisibility(View.VISIBLE);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favoritos, container, false);
+        return myView;
     }
 
 }
+
